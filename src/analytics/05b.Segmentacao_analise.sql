68fef881f5bd39120ac2b401373d8abc765b7b4f
Leandro Correa <leandrocorrea.ep@gmail.com>
2026-02-10 16:53:37 -0300

    feat: feature store de transacional dos usuarios

--- src/analytics/05b.Segmentacao_analise.sql
+++ src/analytics/05b.Segmentacao_analise.sql
@@ -1,9 +1,11 @@
 SELECT
     dtRef ,
     descLifeCycle ,
-    cluster ,
-    COUNT(1) as Usuarios
+    --cluster ,
+    COUNT(1) AS Usuarios
 FROM life_cycle
-WHERE descLifeCycle <> '05-ZUMBI'
-GROUP BY dtRef , descLifeCycle , cluster
-ORDER BY dtRef , descLifeCycle , cluster
+WHERE 
+    descLifeCycle <> '05-ZUMBI'
+    AND dtRef = '2026-01-31'
+GROUP BY dtRef , descLifeCycle -- , cluster
+ORDER BY dtRef , descLifeCycle -- , cluster
